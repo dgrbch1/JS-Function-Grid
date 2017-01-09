bb3f07148ce9f52423b2dadcfe7c83bb65213c2b
Christopher Gonzalez <wqweqweas@gmail.com>
2017-01-09 22:19:51 +0000

    smileface

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //Write your function calls below this line
+setColor(2,0, "purple");
+setColor(3,1,"green");
+setColor(0,3,"red");
+setColor(0,6,"pink");
+setColor(4,2,"green");
+setColor(5,3, "gold");
+setColor(5,4,"brown");
+setColor(5,5,"green");
+setColor(5,6,"silver");
+setColor(4,7,"blue");
+setColor(3,8,"green");
+setColor(2,9,"purple");
+setColor(2,5,"orange");
+setColor(2,4,"yellow");
